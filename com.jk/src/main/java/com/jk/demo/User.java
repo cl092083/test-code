545bc6cb750fcdff0d6872878a877d86ac590b11
lchen <lchen100@fiberhome.com>
2019-11-20 10:59:02 +0800

    test

---
+++ com.jk/src/main/java/com/jk/demo/User.java
@@ -0,0 +1,47 @@
+package com.jk.demo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private  Integer  userid;
+
+    private  String  username;
+
+    private  Integer userage;
+
+    private  String userhobby;
+
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getUserage() {
+        return userage;
+    }
+
+    public void setUserage(Integer userage) {
+        this.userage = userage;
+    }
+
+    public String getUserhobby() {
+        return userhobby;
+    }
+
+    public void setUserhobby(String userhobby) {
+        this.userhobby = userhobby;
+    }
+}
